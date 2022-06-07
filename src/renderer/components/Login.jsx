162e21762231cdd056dced3be193cdbe368431d2
Aram Hammoudeh <aramhammoudeh@Arams-MacBook-Pro.local>
2022-06-07 17:42:20 -0600

    login system setup

--- src/renderer/components/Login.jsx
+++ src/renderer/components/Login.jsx
@@ -3,16 +3,30 @@ import {
   Container,
   FormControl,
   Input,
+  Paper,
   Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Login(props) {
+  const [signupMode, setSignupMode] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [debug, setDebug] = useState("");
   const _state = props._state;
+
+  function createUser() {
+    axios
+      .post("/users/signup", { username, password })
+      .then((r) => {
+        if (r.data.status && r.data.status == 200) setSignupMode(false);
+      })
+      .catch((e) => {
+        // window.alert(e.data.e);
+        setDebug(e.response.data.error)
+      });
+  }
 
   function loginUser() {
     // TODO - encrypt using bcrypt and pass a JWT to _state
@@ -23,7 +37,7 @@ export default function Login(props) {
           _state.set.loggedIn(true);
         }
       })
-      .catch((e) => console.log(e));
+      .catch((e) => setDebug(e.response.data.error));
   }
 
   function debugUser() {
@@ -38,26 +52,60 @@ export default function Login(props) {
       .catch((e) => console.log(e));
   }
 
-  useEffect(()=> {
-      console.log("log in time")
-  })
+  useEffect(() => {
+    console.log("log in time");
+  });
   return (
     <Container>
-      <Input
-        placeholder="Username"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <Input
-        placeholder="Password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <Button onClick={loginUser} variant="outlined">
-        Login
-      </Button>
+      <Paper>
+        <Input
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <br />
+        <Input
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+
+        {!signupMode ? (
+          <Button onClick={loginUser} variant="outlined">
+            Login
+          </Button>
+        ) : (
+          <div>
+            <Button onClick={createUser} variant="outlined">
+              Sigup
+            </Button>
+
+            <Button
+              onClick={() => {
+                setSignupMode(false);
+              }}
+              variant="outlined"
+            >
+              Back
+            </Button>
+          </div>
+        )}
+      </Paper>
+      {!signupMode ? (
+        <Button
+          onClick={() => {
+            setSignupMode(true);
+          }}
+          variant="outlined"
+        >
+          New
+        </Button>
+      ) : (
+        <></>
+      )}
       <Button onClick={debugUser} variant="outlined">
         DEBUG
       </Button>
